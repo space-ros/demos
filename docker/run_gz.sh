bd477685795a6ada9f7aa8a21f9962939e42670f
tonylitianyu <tonylitianyu@gmail.com>
2022-06-06 23:22:03 -0400

    remove docker for gz

--- docker/run_gz.sh
+++
@@ -1,2 +0,0 @@
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-sudo docker run --net=host -e DISPLAY=$DISPLAY --device=/dev/dri:/dev/dri --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --volume="$SCRIPTPATH/..:/gz_garden/demo:rw" -it gz_garden
---
+++ run_sp.sh
@@ -0,0 +1,2 @@
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+sudo docker run --net=host -e DISPLAY=$DISPLAY --device=/dev/dri:/dev/dri --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --volume="$SCRIPTPATH/..:/root/src/spaceros_demo_ws/src/demo:rw" -it openrobotics/spaceros-demo
