d3c2348a4efe0a91562452b75a58dc47e65f8579
tonylitianyu <tonylitianyu@gmail.com>
2022-06-10 19:26:18 -0400

    modified model path to absolute path in container

--- mars_rover/launch/mars_rover.launch.py
+++ mars_rover/launch/mars_rover.launch.py
@@ -1,16 +1,21 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import TextSubstitution, PathJoinSubstitution, LaunchConfiguration, Command
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 import os
-
+from os import environ
 
 def generate_launch_description():
     ld = LaunchDescription()
 
+    env = {'IGN_GAZEBO_SYSTEM_PLUGIN_PATH':
+           ':'.join([environ.get('IGN_GAZEBO_SYSTEM_PLUGIN_PATH', default=''),
+                     environ.get('LD_LIBRARY_PATH', default='')])}
+
 
     urdf_model_path = os.path.join(FindPackageShare(package='mars_rover').find('mars_rover'), 'urdf/curiosity_mars_rover.xacro')
+    mars_world_model = os.path.join(FindPackageShare(package='mars_rover').find('mars_rover'), 'worlds/mars_curiosity.world')
 
     model_arg = DeclareLaunchArgument(
         name="model", 
@@ -26,7 +31,15 @@ def generate_launch_description():
         ]
     )
 
+    start_world = ExecuteProcess(
+        cmd=['ign gazebo', mars_world_model],
+        output='screen',
+        additional_env=env,
+        shell=True
+    )
+
     ld.add_action(model_arg)
     ld.add_action(test_node)
+    ld.add_action(start_world)
 
     return ld
