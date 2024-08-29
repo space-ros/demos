727d338a3eba41c0c6e02793979967ce8518445b
Mechazo11 <azmyin12@gmail.com>
2024-08-29 15:27:14 -0500

    added Ruff linting, removed unused imports

---
+++ mars_rover/README.md
@@ -0,0 +1,9 @@
+# Curiosity Mars Rover ROS2 package
+
+This packge launches the curiositry mars rover simulation in Gazebo ignition. It is part of the Space ROS demonstration examples.
+
+**TODO**
+
+    * [ ] Description of fixing the .dae file for the ```chassis```
+    * [ ] How I configured it to work with the O3DE project.
+    * [ ] How to instructions
--- mars_rover/launch/mars_rover.launch.py
+++ mars_rover/launch/mars_rover.launch.py
@@ -1,34 +1,32 @@
-from http.server import executable
+"""
+TODO
+
+Author:
+Space ROS
+
+Date: TODO
+Version: N/A
+"""
+
+# Imports
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler, IncludeLaunchDescription
-from launch.substitutions import TextSubstitution, PathJoinSubstitution, LaunchConfiguration, Command
+from launch.actions import ExecuteProcess, RegisterEventHandler
 from launch_ros.actions import Node, SetParameter
-from launch_ros.substitutions import FindPackageShare
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.event_handlers import OnProcessExit, OnExecutionComplete
 import os
 from os import environ
-
-from ament_index_python.packages import get_package_share_directory
-
 import xacro
 
-
-
-# . ../spaceros_ws/install/setup.bash && . ../depends_ws/install/setup.bash
-# rm -rf build install log && colcon build && . install/setup.bash
-
 def generate_launch_description():
-
+    # Define paths to mars_rover and simulation packages
     mars_rover_demos_path = get_package_share_directory('mars_rover')
     mars_rover_models_path = get_package_share_directory('simulation')
-
+    # Define 
     env = {'IGN_GAZEBO_SYSTEM_PLUGIN_PATH':
            ':'.join([environ.get('IGN_GAZEBO_SYSTEM_PLUGIN_PATH', default=''),
                      environ.get('LD_LIBRARY_PATH', default='')]),
            'IGN_GAZEBO_RESOURCE_PATH':
            ':'.join([environ.get('IGN_GAZEBO_RESOURCE_PATH', default=''), mars_rover_demos_path])}
-    
     urdf_model_path = os.path.join(mars_rover_models_path, 'models', 'curiosity_path',
         'urdf', 'curiosity_mars_rover.xacro')
     mars_world_model = os.path.join(mars_rover_demos_path, 'worlds', 'mars_curiosity.world')
