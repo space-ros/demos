ca9a333d2486e6790b63071f114cdb0c31fa1dfb
stevedan <stevedan.o.omodolor@gmail.com>
2024-09-09 17:22:48 +0200

    Fix build path issue(fix issue #36)
    Signed-off-by: stevedan <stevedan.o.omodolor@gmail.com>

--- lunar_pole_exploration_rover/build.sh
+++ lunar_pole_exploration_rover/build.sh
@@ -14,9 +14,14 @@ echo ""
 echo "##### Building Space ROS Demo Docker Image #####"
 echo ""
 
+# get the folder of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOKCERDIR="$DIR/Dockerfile"
+
 docker build -t $ORG/$IMAGE:$TAG \
     --build-arg VCS_REF="$VCS_REF" \
-    --build-arg VERSION="$VERSION" . \
+    --build-arg VERSION="$VERSION" $DIR \
+    -f $DOKCERDIR
 
 echo ""
 echo "##### Done! #####"
