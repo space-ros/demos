ad029e999be733cd6ee0b9be34c61121ac71b759
franklinselva <franklinselva10@gmail.com>
2024-09-09 11:04:47 +0200

    Update curiosity_rover_demo package for managing different simulation environments
    Related to #49

--- curiosity_rover/curiosity_rover_demo/launch/mars_rover.launch.py
+++ curiosity_rover/curiosity_rover_demo/launch/mars_rover.launch.py
@@ -1,22 +1,39 @@
 """Launch the curiosity rover demo."""
 
 from launch import LaunchDescription  # type: ignore
+from launch.actions import DeclareLaunchArgument  # type: ignore
+from launch.substitutions import LaunchConfiguration  # type: ignore
 from launch_ros.actions import Node, SetParameter  # type: ignore
 
 
 def generate_launch_description():
     """Launch the curiosity rover demo."""
 
+    environment_arg = DeclareLaunchArgument(
+        "environment",
+        default_value="gazebosim",
+        description="Environment to run the demo in",
+    )
+
     arm_node = Node(
-        package="curiosity_rover_demo", executable="move_arm", output="screen"
+        package="curiosity_rover_demo",
+        executable="move_arm",
+        output="screen",
+        parameters=[{"environment": LaunchConfiguration("environment")}],
     )
 
     mast_node = Node(
-        package="curiosity_rover_demo", executable="move_mast", output="screen"
+        package="curiosity_rover_demo",
+        executable="move_mast",
+        output="screen",
+        parameters=[{"environment": LaunchConfiguration("environment")}],
     )
 
     wheel_node = Node(
-        package="curiosity_rover_demo", executable="move_wheel", output="screen"
+        package="curiosity_rover_demo",
+        executable="move_wheel",
+        output="screen",
+        parameters=[{"environment": LaunchConfiguration("environment")}],
     )
 
     run_node = Node(
@@ -26,6 +43,7 @@ def generate_launch_description():
     return LaunchDescription(
         [
             SetParameter(name="use_sim_time", value=True),
+            environment_arg,
             arm_node,
             mast_node,
             wheel_node,
