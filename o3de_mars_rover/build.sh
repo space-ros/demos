a656743969e1d41a835c0bb8a85045c6989906e2
Mechazo11 <azmyin12@gmail.com>
2024-09-13 22:54:49 -0500

    build script modified to build three docker images, testing in local machine

--- o3de_mars_rover/build.sh
+++ o3de_mars_rover/build.sh
@@ -17,26 +17,34 @@ DOCKER_DIR="./docker"
 # Exit script with failure if build fails
 set -eo pipefail
 
-
 echo ""
 echo "##### Building moviet2 image #####"
 
-docker build -t $ORG/$IMAGE1:$TAG \
-    -f $DOCKER_DIR/Dockerfile_moveit2 \
+docker build -t $ORG/$IMAGE1:$TAG -f $DOCKER_DIR/Dockerfile_moveit2 \
     --build-arg VCS_REF="$VCS_REF" \
     --build-arg VERSION="$VERSION" .
 
 echo ""
 echo "##### Done! #####"
 
-# echo ""
-# echo "##### Building O3DE + Space ROS Demo Docker Image #####"
-# echo ""
+echo ""
+echo "##### Building space_robots_demo image #####"
 
-# docker build -t $ORG/$IMAGE:$TAG \
-#     --build-arg VCS_REF="$VCS_REF" \
-#     --build-arg VERSION="$VERSION" .
+docker build -t $ORG/$IMAGE2:$TAG -f $DOCKER_DIR/Dockerfile_space_robots \
+    --build-arg VCS_REF="$VCS_REF" \
+    --build-arg VERSION="$VERSION" .
 
-# echo ""
-# echo "##### Done! #####"
+echo ""
+echo "##### Done! #####"
+
+echo ""
+echo "##### Building o3de curiosity rover demo docker image #####"
+echo ""
+
+docker build -t $ORG/$IMAGE3:$TAG -f $DOCKER_DIR/Dockerfile_o3de \
+    --build-arg VCS_REF="$VCS_REF" \
+    --build-arg VERSION="$VERSION" .
+
+echo ""
+echo "##### Done! #####"
 
