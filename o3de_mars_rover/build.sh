48e9b7a5d82b2221d7f8f06d5ea70dbed89b33cd
Mechazo11 <azmyin12@gmail.com>
2024-09-13 22:45:39 -0500

    added three docker files into docker/ dir, testing if they can be chain built

--- o3de_mars_rover/build.sh
+++ o3de_mars_rover/build.sh
@@ -1,23 +1,42 @@
 #!/usr/bin/env bash
 
+# This shell script will build openrobotics/moveit2, openrobotics/space_robot_demo and then
+
 ORG=openrobotics
-IMAGE=o3de_curiosity_docker
+IMAGE1=moveit2
+IMAGE2=space_robot_demo
+IMAGE3=o3de_curiosity_docker
 TAG=latest
 
 VCS_REF=""
 VERSION=preview
 
+# Directory where Dockerfiles are stored
+DOCKER_DIR="./docker"
+
 # Exit script with failure if build fails
 set -eo pipefail
 
-echo ""
-echo "##### Building O3DE + Space ROS Demo Docker Image #####"
-echo ""
 
-docker build -t $ORG/$IMAGE:$TAG \
+echo ""
+echo "##### Building moviet2 image #####"
+
+docker build -t $ORG/$IMAGE1:$TAG \
+    -f $DOCKER_DIR/Dockerfile_moveit2 \
     --build-arg VCS_REF="$VCS_REF" \
     --build-arg VERSION="$VERSION" .
 
 echo ""
 echo "##### Done! #####"
+
+# echo ""
+# echo "##### Building O3DE + Space ROS Demo Docker Image #####"
+# echo ""
+
+# docker build -t $ORG/$IMAGE:$TAG \
+#     --build-arg VCS_REF="$VCS_REF" \
+#     --build-arg VERSION="$VERSION" .
+
+# echo ""
+# echo "##### Done! #####"
 
