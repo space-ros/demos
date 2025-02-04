3458c2a61d4891fce1e4e06dc9db44a71bae705e
Hiroki Kato <yuyuqq@gmail.com>
2025-02-04 21:22:27 +0900

    added racs2_demos_on_spaceros jaxa's demo (Issue #26)

---
+++ racs2_demos_on_spaceros/cFS/apps/run_app/fsw/src/run_app_events.h
@@ -0,0 +1,13 @@
+#ifndef _run_app_events_h_
+#define _run_app_events_h_
+
+
+#define SAMPLE_RESERVED_EID              20
+#define SAMPLE_STARTUP_INF_EID           21 
+#define SAMPLE_COMMAND_ERR_EID           22
+#define SAMPLE_COMMANDNOP_INF_EID        23 
+#define SAMPLE_COMMANDRST_INF_EID        24
+#define SAMPLE_INVALID_MSGID_ERR_EID     25 
+#define SAMPLE_LEN_ERR_EID               26 
+
+#endif /* _run_app_events_h_ */
