f9d4218d9ca6d67159303b3b9ca82ea1a0f3f976
Matt Hansen <hansenmattk@gmail.com>
2025-07-25 13:50:11 -0700

    Log build steps as debug info (#94)
    Adds echo statements to the build.sh file to log each build step

--- ros_trick/build.sh
+++ ros_trick/build.sh
@@ -13,9 +13,13 @@ if [ ! -d ${SPACEROS_DOCKER_REPO_PATH} ]; then
     mkdir -p ${SPACEROS_DOCKER_REPO_PATH}
     git clone https://github.com/space-ros/docker.git ${SPACEROS_DOCKER_REPO_PATH}
 fi
+echo "*** Building moveit2 ***"
 cd ${SPACEROS_DOCKER_REPO_PATH}/moveit2 && ./build.sh
+echo "*** Building space_robots ***"
 cd ${SPACEROS_DOCKER_REPO_PATH}/space_robots && ./build.sh
 cd ${CURRENT_PATH}
 
+echo "*** Building ros_trick_bridge.Dockerfile ***"
 docker build -t ${ROS_TRICK_BRIDGE_IMAGE_NAME}:${TAG} -f ./ros_trick_bridge.Dockerfile .
+echo "*** Building canadarm_ros_trick_demo.Dockerfile ***"
 docker build -t ${CANADARM_ROS_TRICK_DEMO_NAME}:${TAG} -f ./canadarm_ros_trick_demo.Dockerfile .
