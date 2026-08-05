1ca237c6d360a3195fa65e5b18bbcc6c5a7712d3
Alexey Simonov <alexey.simonov@gmail.com>
2026-08-05 15:25:59 +0400

    nav2_demo: Update Dockerfile for Jazzy (#133).

--- nav2_demo/entrypoint.sh
+++ nav2_demo/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -e
 
-# Setup the Navigation2 environment
+# Underlay: apt RViz (present on osrf/space-ros-nav2)
+if [ -f "/opt/ros/${ROS_DISTRO}/setup.bash" ]; then
+  # shellcheck disable=SC1090
+  source "/opt/ros/${ROS_DISTRO}/setup.bash"
+fi
+
+# Overlay: source-built Space ROS Nav2 workspace
 source "/home/spaceros-user/nav2_ws/install/setup.bash"
+
+# Overlay: demo RViz launch/config + source-built nav2_rviz_plugins
+if [ -f "/home/spaceros-user/nav2_demo_ws/install/setup.bash" ]; then
+  # shellcheck disable=SC1091
+  source "/home/spaceros-user/nav2_demo_ws/install/setup.bash"
+fi
 
 exec "$@"
